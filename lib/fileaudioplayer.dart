75237b69a5a9ac8acee30036b1ec1be0b092ea07
Matteo Ricupero <matteo@MacBook-Pro-di-Matteo.local>
2020-03-22 15:19:01 +0100

    update

--- lib/fileaudioplayer.dart
+++ lib/fileaudioplayer.dart
@@ -5,8 +5,37 @@ import 'package:flutter/services.dart';
 class Fileaudioplayer {
   static const MethodChannel _channel = const MethodChannel('fileaudioplayer');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  Fileaudioplayer();
+
+  Future<void> start(String path) async {
+    try {
+      await _channel.invokeMethod("start", path);
+    } on PlatformException catch (e) {
+      print("Stream start error : $e");
+    }
+  }
+
+  Future<void> stop() async {
+    try {
+      await _channel.invokeMethod("stop");
+    } on PlatformException catch (e) {
+      print("Stream stop error : $e");
+    }
+  }
+
+  Future<void> pause() async {
+    try {
+      await _channel.invokeMethod("pause");
+    } on PlatformException catch (e) {
+      print("Stream pause error : $e");
+    }
+  }
+
+  Future<void> resume() async {
+    try {
+      await _channel.invokeMethod("resume");
+    } on PlatformException catch (e) {
+      print("Stream resume error : $e");
+    }
   }
 }

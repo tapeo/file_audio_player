6b5e30d85919c7f406c5d56bde5f5ccac17fd2ce
Matteo Ricupero <matteo@MacBook-Pro-di-Matteo.local>
2020-03-22 15:12:04 +0100

    update

--- lib/fileaudioplayer.dart
+++ lib/fileaudioplayer.dart
@@ -3,8 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Fileaudioplayer {
-  static const MethodChannel _channel =
-      const MethodChannel('fileaudioplayer');
+  static const MethodChannel _channel = const MethodChannel('fileaudioplayer');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
